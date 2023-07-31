60f51ff0407255309774d910ad662c0685b221f4
maxmart26 <torgue.maxime@gmail.com>
2023-07-31 10:19:48 +0200

    end

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,15 @@
 import "../../style.css"
 import logo from '../../datas/LOGO.png'
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 
 function Header(){
+    const location = useLocation();
     return (
         <header>
             <img className='logo' src={logo} alt='logo'/>
             <nav>
                     <Link to="/">Accueil</Link>
-                    <Link to="/a-propos">A Propos</Link>
+                    <Link className={location.pathname === "/a-propos" ? "active" : ""} to="/a-propos">A Propos</Link>
             </nav>
         </header>
     )
