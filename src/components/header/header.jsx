98c0b368d8ef20e3529b6ca1afd0e5b157f1c291
maxmart26 <torgue.maxime@gmail.com>
2023-07-31 14:16:56 +0200

    end

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,25 @@
+import React, { useState } from 'react';
 import "../../style.css"
 import logo from '../../datas/LOGO.png'
 import {Link, useLocation} from "react-router-dom"
 
-function Header(){
+const Header = () => {
     const location = useLocation();
+    const [activeLink, setActiveLink] = useState(location.pathname); 
+  
+    const handleLinkClick = (path) => {
+      setActiveLink(path);
+    };
+  
     return (
-        <header>
-            <img className='logo' src={logo} alt='logo'/>
-            <nav>
-                    <Link to="/">Accueil</Link>
-                    <Link className={location.pathname === "/a-propos" ? "active" : ""} to="/a-propos">A Propos</Link>
-            </nav>
-        </header>
-    )
-}
-export default Header
+      <header>
+        <img className='logo' src={logo} alt='logo'/>
+        <nav>
+          <Link className={activeLink === "/" ? "active" : ""}to="/"onClick={() => handleLinkClick("/")}>Accueil</Link>
+          <Link className={activeLink === "/a-propos" ? "active" : ""}to="/a-propos"onClick={() => handleLinkClick("/a-propos")}>A Propos</Link>
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
